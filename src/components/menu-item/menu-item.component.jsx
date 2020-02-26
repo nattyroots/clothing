b528c0094936e7b61584fec4100ebf6c81b3a28b
nattyroots <nattyroots96@gmail.com>
2020-02-26 12:46:40 +0200

    add hower on the Menu items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title = "default value", imageUrl, key, size }) => {
-    return (<div style={{
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{
         backgroundImage: `url(${imageUrl})`
-    }} className={`${size} menu-item`} >
-        <div className='content'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'>SHOP NOW</span>
-        </div>
-    </div >)
-}
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
+    </div>
+  </div>
+);
+
 export default MenuItem;
